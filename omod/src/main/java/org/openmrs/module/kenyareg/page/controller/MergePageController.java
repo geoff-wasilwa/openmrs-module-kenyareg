e7a814f47c35096cda46c5e318664b75bfb29266
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-09-15 13:21:57 +0300

    Redirect to Registration home when session expires

--- omod/src/main/java/org/openmrs/module/kenyareg/page/controller/MergePageController.java
+++ omod/src/main/java/org/openmrs/module/kenyareg/page/controller/MergePageController.java
@@ -1,7 +1,10 @@
 package org.openmrs.module.kenyareg.page.controller;
 
+import org.apache.commons.lang.StringUtils;
+import org.go2itech.oecui.data.RequestResult;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.ui.framework.session.Session;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class MergePageController {
@@ -9,9 +12,12 @@ public class MergePageController {
 	public String get(
 			@RequestParam(value = "lpiUid", required = false) String lpiUid,
 			@RequestParam(value = "mpiUid", required = false) String mpiUid,
-			PageModel model, UiUtils ui) {
-		if (lpiUid == null && mpiUid == null) {
-			return "redirect:" + ui.pageLink("kenyareg", "basicSearch");
+			PageModel model, UiUtils ui, Session session) {
+		RequestResult lpiResult = session.getAttribute("lpiResult", RequestResult.class);
+		RequestResult mpiResult = session.getAttribute("mpiResult", RequestResult.class);
+		if (StringUtils.isNotBlank(lpiUid) && lpiResult == null 
+				|| StringUtils.isNotBlank(mpiUid) && mpiResult == null) {
+			return "redirect:" + ui.pageLinkWithoutContextPath("kenyareg", "registryHome", null);
 		}
 		model.addAttribute("lpiUid", lpiUid);
 		model.addAttribute("mpiUid", mpiUid);
